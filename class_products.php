059f42dd77ad0d61deea394b7257ce43be7bb70e
HasanHira <qubitgraphix@gmail.com>
2023-05-01 23:19:19 +0600

    Ready

--- class_products.php
+++ class_products.php
@@ -21,6 +21,25 @@ class Product {
     }
 }
 
-
 $Zingphone = new Product( "Zing Phone", 299, "Durable, Stain resistant, Permanent color" );
 $Zingphone->about_product();
+
+
+//class
+class Human{
+    public $name;
+    function sayHi(){
+        echo "Salam\n";
+        $this->sayName();
+    }
+
+    function sayName(){
+        echo "My name is {$this->name}";
+    }
+}
+
+//object
+$h1 = new Human();
+$h2 = new Human();
+echo $h1->name = "Elias";
+$h2->name = "Hasin";
